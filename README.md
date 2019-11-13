3f021a3e9f6f44e93ae00416bf4d88de02e998e8
Bruno Ferreira <brunoferreira_@outlook.com>
2019-11-12 23:14:47 -0300

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,15 @@
-# vue-vuex
+# Vuex
 
-## Project setup
+Projeto simples para entender o uso do gerenciador de estado Vuex
+
+## Instalação dependências
+
 ```
 yarn install
 ```
 
-### Compiles and hot-reloads for development
+### Compilação e auto-reload
+
 ```
 yarn run serve
 ```
-
-### Compiles and minifies for production
-```
-yarn run build
-```
-
-### Run your tests
-```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <div id="app">{{ count }}</div>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increment() {
+      return this.$store.commit("increment");
+    },
+    decrement() {
+      return this.$store.commit("decrement");
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
